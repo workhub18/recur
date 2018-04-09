248e0675142ad245bfa341902a962d8f2ee3a3d8
workhub18 <workhub18@users.noreply.gitub.com>
2018-04-09 11:53:37 -0400

    Added recursion file

---
+++ recursion.cpp
@@ -0,0 +1,24 @@
+#include<iostream> 
+#include<string>
+
+
+bool isPalindrome(string s)
+{
+	if()
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+int main()
+{
+  string word;
+  cout<<"Enter a word to check "<<endl;
+  cin>>word;
+  isPalindrome(word);
+
+return 0;
+}
